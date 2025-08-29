5cc6b7d5918f9d1d305da78380297bf262ec5162
Sucerquia <danielsucerquia@hotmail.com>
2025-08-29 11:10:22 +0200

    UPD: package information

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,7 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../src/'))
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -6,32 +10,33 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-#project = 'SITH'
-copyright = '2022, Mikaela Farrugia, Daniel Sucerquia'
-author = 'Mikaela Farrugia, Daniel Sucerquia'
-release = '0.1.1'
+project = 'SITH'
+copyright = '2023, Daniel Sucerquia, Mikaela Farrugia'
+author = 'Daniel Sucerquia, Mikaela Farrugia'
+release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon', 'sphinx_rtd_theme']
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.coverage',
+              'sphinx.ext.napoleon',
+              'sphinx_rtd_theme']
 
+html_theme = 'sphinx_rtd_theme'
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../src/'))
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 #remote_theme: rundocs/jekyll-rtd-theme
+html_static_path = ['_static']
+html_favicon = '_static/favicon.ico'
+html_title = "SITH"
 
-html_theme = 'sphinx_rtd_theme'
 html_theme_options = {
     'display_version' : True,
     'style_external_links' : True
 }
-html_static_path = ['_static']
