c3a82aa559994d0991c1f3179f98b7d1e7f32d16
Sucerquia <danielsucerquia@hotmail.com>
2025-09-01 18:00:57 +0200

    DOC: flags in stretching

--- src/sith/g09_stretching/stretching.sh
+++ src/sith/g09_stretching/stretching.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 #SBATCH -N 1                   # number of nodes
-#SBATCH -n 9
 #SBATCH --cpus-per-task=1
 #SBATCH -t 24:00:00
 #SBATCH --output=%x-%j.o
@@ -12,18 +11,22 @@
 print_help() {
 echo "
 This tool obtains the stretched configurations of a molecule by increasing the
-distance between two atoms, constraining and optimizing at every step
+distance between two atoms, constraining and optimizing at every step.
 
   -b  <number_of_breakages=1> The simulation will run until get this number of
       ruptures.
+  -c  Use this flag to run in a cluster set with slurm. In that case, the
+      of processors is equal to the number of cores asked in the submission of
+      the job. When this flag is present, -p is ignored.
   -e  <extend_method=0> index of stretching method. To see the options, use
       'sith change_distance -h' to see the order.
       carbons of the capping groups
   -i  <index1,index2> indexes of the atoms to use for increasing the distance.
-  -m  <molecule> One letter code of the amino acids forming the molecules. In
-      this directory, a file called <molecule>-stretched00.pdb has to exist.
   -l  <xc,base=bmk,6-31+g> evel of DFT theory.
-  -r  restart stretching. In this case, this conde must be executed from
+  -m  <molecule> molecule name. In this directory, a file called
+      <molecule>-stretched00.pdb must exist.
+  -p  <processors=8> number of processors per gaussian job.
+  -r  restart stretching. In this case, this code must be executed from
       the molecule's directory.
   -s  <size[A]=0.2> Size of the step that increases the distances at each step.
 
@@ -44,17 +47,19 @@ size=0.2
 verbose='false'
 indexes=''
 level='bmk,6-31+g'
-while getopts 'b:ce:i:l:m:rs:vh' flag; do
+cluster='false'
+while getopts 'b:ce:i:l:m:p:rs:vh' flag; do
   case "${flag}" in
     b) breakages=${OPTARG} ;;
     c) cluster='true' ;;
     e) extend_method=${OPTARG} ;;
     i) indexes=${OPTARG} ;;
-    m) mol=${OPTARG} ;;
     l) level=${OPTARG} ;;
+    m) mol=${OPTARG} ;;
+    p) n_processors=${OPTARG} ;;
     r) restart='true' ;;
     s) size=${OPTARG} ;;
-    
+
     v) verbose='true' ;;
     h) print_help ;;
     *) echo "for usage check: sith <function> -h" >&2 ; exit 1 ;;
@@ -63,7 +68,6 @@ done
 
 source "$(sith basics -path)" STRETCHING $verbose
 
-n_processors=8
 if $cluster
 then
   load_modules
