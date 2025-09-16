e7961f830430789513696ae500943ad871823a25
Sucerquia <danielsucerquia@hotmail.com>
2025-09-16 15:43:34 +0200

    DOC: documentation of swap_atoms_in_com

--- src/sith/g09_stretching/swap_atoms_in_com.sh
+++ src/sith/g09_stretching/swap_atoms_in_com.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+
+# ---- functions --------------------------------------------------------------
 print_help() {
 echo "
-# TODO: repair
+Permutes the incides of two atoms in com file where the molecule is defined as
+a z-matrix.
 
-Use this template to create your scripts with a standard structure
+  -a  <atom1> index of one of the atoms to be permuted. 
+  -b  <atom2> index of the other atom to be permuted.
+  -f  <file.com> input gaussian file with the z-matrix already defined.
 
   -v  verbose.
   -h  prints this message.
@@ -11,11 +16,18 @@ Use this template to create your scripts with a standard structure
 exit 0
 }
 
+permutate_pattern() {
+    pattern1="$1"
+    pattern2="$2"
+    fil="$3"
+    # permutate index in connectivity
+    sed -i "s/$pattern1/tmp_pattern1/g" $fil
+    sed -i "s/$pattern2/$pattern1/g" $fil
+    sed -i "s/tmp_pattern1/$pattern2/g" $fil
+}
+
 # ----- set up starts ---------------------------------------------------------
 # General variables
-def_var="inse here your default"
-cascade='false'
-verbose='false'
 while getopts 'a:b:f:vh' flag;
 do
   case "${flag}" in
@@ -29,16 +41,6 @@ do
   esac
 done
 
-permutate_pattern() {
-    pattern1="$1"
-    pattern2="$2"
-    fil="$3"
-    # permutate index in connectivity
-    sed -i "s/$pattern1/tmp_pattern1/g" $fil
-    sed -i "s/$pattern2/$pattern1/g" $fil
-    sed -i "s/tmp_pattern1/$pattern2/g" $fil
-}
-
 # change lines
 def_atom1=$( grep ",R$atom1," $file )
 def_atom2=$( grep ",R$atom2," $file )
@@ -50,7 +52,7 @@ permutate_pattern "$def_atom1" "$def_atom2" $file
 permutate_pattern ",$atom1," ",$atom2," $file
 permutate_pattern "\ $atom1\ " "\ $atom2\ " $file
 
-# permutate indez in R,A,D
+# permutate index in R,A,D
 permutate_pattern "R$atom1" "R$atom2" $file
 permutate_pattern "A$atom1" "A$atom2" $file
 permutate_pattern "D$atom1" "D$atom2" $file
