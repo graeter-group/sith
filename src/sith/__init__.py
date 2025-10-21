452771324c35686f4087fd906ac576ae3d56859f
Sucerquia <danielsucerquia@hotmail.com>
2025-10-21 17:58:46 +0200

    ENH: sith imports SITH

--- src/sith/__init__.py
+++ src/sith/__init__.py
@@ -1,5 +1,8 @@
+from .SITH import SITH
 from pathlib import Path
 
+
+SITH.__module__ = __name__
 
 class variables4tests:
     resources = Path(__file__).parent / '..' / '..' / 'tests' / 'resources'
